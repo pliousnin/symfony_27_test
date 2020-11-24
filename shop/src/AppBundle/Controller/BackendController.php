96fc33bcc5d1d7e10f1777463384f923169a586a
Pawel Pliousnin <pawel.pliousnin@even-on-sunday.com>
2020-11-24 17:18:00 +0100

    Controller and Template

---
+++ shop/src/AppBundle/Controller/BackendController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController2 extends Controller
+{
+    /**
+     * @Route("/2", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        $number = 5;
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ));
+    }
+}
