cc9cd71f67b8cc21e6d1adf76a03130bf63ed1e4
Pawel Pliousnin <pawel.pliousnin@even-on-sunday.com>
2020-12-02 15:04:13 +0100

    set Q

--- shop/src/SearchBundle/Controller/SearchController.php
+++ shop/src/SearchBundle/Controller/SearchController.php
@@ -2,7 +2,6 @@
 // src/SearchBundle/Controller/SearchController.php
 namespace SearchBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
--- shop/src/SearchBundle/Helper/SearchHelper.php
+++ shop/src/SearchBundle/Helper/SearchHelper.php
@@ -8,6 +8,7 @@ use AppBundle\Helper\BaseHelper;
 class SearchHelper extends BaseHelper
 {
     private $q;
+    private $q_array;
     private $response = [];
     private $html;
     private $debug = true;
@@ -20,7 +21,8 @@ class SearchHelper extends BaseHelper
     }
 
     public function setQ($q){
-        $this->q = $q;
+        $this->q = preg_replace('/\s+/', ' ',$q);
+        $this->q_array = array_unique(explode(' ', $this->q));
     }
 
     public function init(){
@@ -31,6 +33,7 @@ class SearchHelper extends BaseHelper
         $this->response['html'] = $this->html;
         if ($this->debug){
             $this->response['q'] = $this->q;
+            $this->response['q_array'] = $this->q_array;
         }
     }
 
